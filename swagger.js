ae65fcb16be909cbdbb46b37cf7a78ffbe0f20ce
MohamedHesham2024 <mohmedhesham2024@gmail.com>
2025-07-16 12:51:30 +0300

    add server configuration for Swagger API documentation

--- swagger.js
+++ swagger.js
@@ -7,6 +7,12 @@ const options = {
       title: "Project API",
       version: "1.0.0",
     },
+     servers: [
+      {
+        url: "https://real-estate-backend-pi-steel.vercel.app", // 👈 بدون /api
+        description: "Vercel deployment",
+      },
+    ],
   },
   apis: ["./routes/*.js"],
 };
